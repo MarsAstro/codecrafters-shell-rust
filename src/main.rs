1e88f8d08cef718b4ababd8c91e9a1f58f4c2584
Marius Stubberud <marius.stubberud.92@gmail.com>
2026-01-25 20:49:09 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,12 @@ fn main() {
         if !words.is_empty() && words[0] == "echo" {
             let args = words[1..].join(" ");
             println!("{args}");
+        } else if !words.is_empty() && words[0] == "type" {
+            if words[1] == "echo" || words[1] == "type" || words[1] == "exit" {
+                println!("{} is a shell builtin", words[1]);
+            } else {
+                println!("{}: not found", words[1]);
+            }
         } else {
             println!("{command}: command not found");
         }
